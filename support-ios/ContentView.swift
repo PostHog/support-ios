ea5a3d5ae64ba0fe6ab11bcc60a3ee835dc572f6
Joshua Ordehi <45109738+ordehi@users.noreply.github.com>
2025-04-23 11:45:44 -0400

    first commit

---
+++ support-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  support-ios
+//
+//  Created by Joshua Ordehi on 23/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ support-ios/support_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  support_iosApp.swift
+//  support-ios
+//
+//  Created by Joshua Ordehi on 23/4/25.
+//
+
+import SwiftUI
+
+@main
+struct support_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
