006e0a78fc325d698ab7a582d77837817f5b8fa0
Joshua Ordehi <45109738+ordehi@users.noreply.github.com>
2025-04-23 11:54:21 -0400

    implement posthog sdk

--- support-ios/support_iosApp.swift
+++ support-ios/support_iosApp.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import PostHog
 
 @main
 struct support_iosApp: App {
+    init() {
+        let config = PostHogConfig(apiKey: "phc_ddA554Xlja4bfLInShnZnM6b7d3Op1aQAaieWzw3oz7", host: "https://us.i.posthog.com")
+        PostHogSDK.shared.setup(config)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
